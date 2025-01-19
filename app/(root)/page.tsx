ae510b6a9a40dba1966b64ea98229377ee893dce
Jimike110 <oladoyemike@gmail.com>
2025-01-19 16:10:00 +0300

    Implementing bank cards display on right side bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Michael" };
+  const loggedIn = { firstName: "Michael", lastName: "Oladoye", email: 'oladoyemike@gmail.com' };
   return (
     <section className="home">
       <div className="home-content">
@@ -16,12 +17,15 @@ const Home = () => {
           ></HeaderBox>
 
           <TotalBalanceBox
-          accounts ={[]}
-          totalBanks={1}
-          totalCurrentBalance={1250.35}>
-          </TotalBalanceBox>
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          ></TotalBalanceBox>
         </header>
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance: 123.50}, {currentBalance: 500.50}]}></RightSidebar>
     </section>
   );
 };
