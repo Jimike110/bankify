3c3760fa54bb51c738a3bdb4be9b0688f73d2521
Jimike110 <oladoyemike@gmail.com>
2025-06-11 16:46:44 +0300

    Improved auth components; made custom input components

---
+++ components/CustomInput.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { FormControl, FormField, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
+import { Control } from 'react-hook-form'
+import { authFormSchema } from "@/lib/utils";
+import { z } from "zod";
+
+interface CustomInput {
+    control: Control<z.infer<typeof authFormSchema>>,
+    name: "email" | "password",
+    label: string,
+    placeholder: string
+}
+
+const CustomInput = ({ control, name, label, placeholder }: CustomInput) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <div className="form-item">
+          <FormLabel className="form-label">{label}</FormLabel>
+          <div className="flex w-full flex-col">
+            <FormControl>
+              <Input
+                className="input-class"
+                placeholder={placeholder}
+                {...field}
+              ></Input>
+            </FormControl>
+            <FormMessage className="form-message mt-2"></FormMessage>
+          </div>
+        </div>
+      )}
+    />
+  );
+};
+
+export default CustomInput;
