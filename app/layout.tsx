9ad50e370186cf4d15740a29d89cdbb41d84ef82
Jimike110 <oladoyemike@gmail.com>
2025-06-12 12:11:20 +0300

    Installed and implemented Sentry

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,20 +2,28 @@ import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
-const ibmPlexSerif = IBM_Plex_Serif({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-ibm-plex-serif'
-})
+import * as Sentry from "@sentry/nextjs";
 
-export const metadata: Metadata = {
-  title: "Bankify",
-  description: "Bankify is a modern banking platform for everyone.",
-  icons: {
-    icon: 'icons/logo.svg'
-  }
-};
+// Add or edit your "generateMetadata" to include the Sentry trace data:
+export function generateMetadata(): Metadata {
+  return {
+    title: "Bankify",
+    description: "Bankify is a modern banking platform for everyone.",
+    icons: {
+      icon: "icons/logo.svg",
+    },
+    other: {
+      ...Sentry.getTraceData(),
+    },
+  };
+}
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-ibm-plex-serif",
+});
 
 export default function RootLayout({
   children,
@@ -24,7 +32,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
